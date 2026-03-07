1da8c116391b361a711f3ef94fd3771a0b04585e
DT-Around-the-World <dtaroundtheworld@yahoo.com>
2026-03-07 13:44:23 -0500

    Create movies.js
    create data

---
+++ movies.js
@@ -0,0 +1,49 @@
+const movies = [
+
+{
+country:"Afghanistan",
+code:"AF",
+movie:"Opium War",
+link:"https://en.wikipedia.org/wiki/Opium_War_(2008_film)",
+  stream:"https://www.youtube.com/watch?v=6Nl1oQL6AnA",
+poster:"https://upload.wikimedia.org/wikipedia/en/c/c6/Opium_war_poster.jpg",
+rating:3,
+review:"Low budget symbolic movie. Recommend.",
+watched:true
+}, {
+country:"Albania",
+code:"AL",
+movie:"Slogans",
+link:"https://en.wikipedia.org/wiki/Slogans_(film)",
+  stream:"https://www.youtube.com/watch?v=6Nl1oQL6AnA",
+poster:"https://upload.wikimedia.org/wikipedia/en/7/7e/Slogans_%28film%29.jpg",
+rating:0,
+review:"",
+watched:false
+},
+  {
+country:"Algeria",
+code:"DZ",
+movie:"Rachida",
+link:"https://en.wikipedia.org/wiki/Rachida",
+  stream:"https://www.youtube.com/watch?v=N1sXEVAlXWo",
+poster:"https://upload.wikimedia.org/wikipedia/en/7/7e/Slogans_%28film%29.jpg",
+rating:0,
+review:"",
+watched:false
+},
+
+    {
+country:"American Samoa",
+code:"AS",
+movie:"SEKI A OE",
+link:"https://en.wikipedia.org/wiki/Seki_A_Oe:_A_Crazy_Samoan_Love_Story",
+  stream:"https://www.youtube.com/watch?v=mFsl8RGM-eE",
+poster:"https://upload.wikimedia.org/wikipedia/en/6/69/Seki_A_Oe_film_poster.jpg",
+rating:0,
+review:"",
+watched:false
+},
+  
+
+]
